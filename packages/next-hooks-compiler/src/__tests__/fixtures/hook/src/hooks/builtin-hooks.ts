6b9b1c36855eb9804ce5fd1177280aa278679f2f
Lxxyx <Lxxyxzj@gmail.com>
2020-08-17 21:42:31 +0800

    ✨ feat(usePlugin): support usePlugin hooks

--- packages/next-hooks-compiler/src/__tests__/fixtures/hook/src/hooks/builtin-hooks.ts
+++ packages/next-hooks-compiler/src/__tests__/fixtures/hook/src/hooks/builtin-hooks.ts
@@ -1,8 +1,9 @@
-import { useContext, useInject, useConfig, useLogger } from '@midwayjs/hooks'
+import { useContext, useInject, useConfig, useLogger, usePlugin } from '@midwayjs/hooks'
 
 export function useBuiltinHooks() {
   useConfig()
   useLogger()
   useContext()
   useInject('')
+  usePlugin('')
 }
