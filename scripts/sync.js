d7fadf9c5dd00b808024f84b8137f0efc7a4a1c5
Lxxyx <Lxxyxzj@gmail.com>
2020-08-17 09:52:55 +0800

    ✨ feat(scripts): faster sync scripts

--- scripts/sync.js
+++ scripts/sync.js
@@ -4,17 +4,26 @@ const { resolve } = require('path')
 
 const originData = execSync('npx lerna ls --json').toString()
 const data = JSON.parse(originData)
+const finished = []
 
 async function syncPackage(pkg) {
-  console.log(`start sync ${pkg}`)
   await execa('tnpm', ['sync', pkg])
-  console.log(`${pkg} sync finished`)
+  finished.push(pkg)
+  console.log(`[${finished.length}/${data.length}] ${pkg} sync finished`)
 }
 
 async function sync() {
-  const task = data.map((item) => syncPackage(item.name))
+  console.log('\n=== start sync ===\n')
+  const packages = data.map((item) => item.name)
+
+  console.log(`sync ${packages.length} packages:\n${packages.join('\n')}\n`)
+  const task = packages.map((pkg) => syncPackage(pkg))
   await Promise.all(task)
-  await execa('node', [resolve(__dirname, 'sync_status.js')])
+
+  console.log('\n=== check sync status ===\n')
+  await execa('node', [resolve(__dirname, 'sync_status.js')], { stdio: 'inherit' })
+
+  console.log('\n=== sync finished ===')
   process.exit(1)
 }
 
