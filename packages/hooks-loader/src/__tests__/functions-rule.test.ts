e9b0b31d8bb1e5ca9573b58167de0a4b0c377ab4
Lxxyx <Lxxyxzj@gmail.com>
2020-10-18 21:35:20 +0800

    ✨ feat(functionsRule): move routeUnderscore to functionsRule.underscore

---
+++ packages/hooks-loader/src/__tests__/functions-rule.test.ts
@@ -0,0 +1,24 @@
+import { resolve } from 'path'
+import compiler from './compiler'
+import webpack from 'webpack'
+
+const root = resolve(__dirname, './fixtures/functions-rule')
+
+const resolveEntry = (path: string) => {
+  return resolve(__dirname, root, 'src/apis/', path)
+}
+const getOutput = (stats: webpack.Stats) => {
+  return stats.toJson().modules[2].source
+}
+
+test('Compile render', async () => {
+  const stats = await compiler(resolveEntry('render/index.ts'), root)
+  const output = getOutput(stats)
+  expect(output).toMatchSnapshot()
+})
+
+test('Compile lambda', async () => {
+  const stats = await compiler(resolveEntry('lambda/index.ts'), root)
+  const output = getOutput(stats)
+  expect(output).toMatchSnapshot()
+})
