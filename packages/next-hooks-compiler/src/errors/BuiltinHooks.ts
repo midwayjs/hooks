eacc52e114c5a3d2ad99855c7e8679a0e00b5e44
Lxxyx <Lxxyxzj@gmail.com>
2020-10-18 18:10:16 +0800

    🔨 refactor: use getSourceFilePath

--- packages/next-hooks-compiler/src/errors/BuiltinHooks.ts
+++ packages/next-hooks-compiler/src/errors/BuiltinHooks.ts
@@ -1,13 +1,14 @@
 import ts from 'typescript'
 import { CompileError } from './Compile'
 import { BuiltinHooks } from '../const'
+import { getSourceFilePath } from '../util'
 
 export class BuiltinHooksError extends CompileError {
   constructor(ref: ts.Identifier) {
     const messages = [
       `编译模式下仅支持转换以下内置 Hooks：${BuiltinHooks.join(',')}`,
       `引用变量：${ref.getText()}`,
-      `文件地址：${ref.getSourceFile().fileName}`,
+      `文件地址：${getSourceFilePath(ref)}`,
     ]
     super(messages.join('\n'))
     this.name = 'BuiltinHooksError'
--- packages/next-hooks-compiler/src/errors/InvalidReference.ts
+++ packages/next-hooks-compiler/src/errors/InvalidReference.ts
@@ -1,12 +1,13 @@
 import { CompileError } from './Compile'
 import ts from 'typescript'
+import { getSourceFilePath } from '../util'
 
 export class InvalidReferenceError extends CompileError {
   constructor(ref: ts.Node) {
     const messages = [
       `只在导出的接口与自定义 Hook 中调用 Hook`,
       `引用变量：${ref.getText()}`,
-      `文件地址：${ref.getSourceFile().fileName}`,
+      `文件地址：${getSourceFilePath(ref)}`,
       '文档: https://www.yuque.com/midwayjs/faas/qrsykh#pTyCv',
     ]
     super(messages.join('\n'))
