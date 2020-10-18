5541bb36a9d890cc94cf2b1d2609cbd36c9322b8
Lxxyx <Lxxyxzj@gmail.com>
2020-10-18 17:35:47 +0800

    ✨ feat(features): hooks-loader support routeUnderscore

--- packages/hooks-loader/src/__tests__/compiler.ts
+++ packages/hooks-loader/src/__tests__/compiler.ts
@@ -2,9 +2,7 @@ import memoryfs from 'memory-fs'
 import path from 'path'
 import webpack from 'webpack'
 
-const root = path.resolve(__dirname, './fixtures')
-
-export default (fixture: string): Promise<webpack.Stats> => {
+export default (fixture: string, root: string): Promise<webpack.Stats> => {
   const compiler = webpack({
     context: root,
     entry: fixture,
