3a11a9141632fbd450b1bb804a540cc5cd0e8902
Lxxyx <Lxxyxzj@gmail.com>
2020-11-05 17:02:32 +0800

    ✨ feat(error): better error output

--- packages/next-hooks-compiler/src/errors/BuiltinHooks.ts
+++ packages/next-hooks-compiler/src/errors/BuiltinHooks.ts
@@ -1,15 +1,16 @@
 import ts from 'typescript'
+import { relative } from 'upath'
+import { helper } from '..'
 import { BuiltinHooks } from '../const'
 import { getSourceFilePath } from '../util'
+import { buildErrorCodeFrame } from './code-frame'
 
 export class BuiltinHooksError extends Error {
   constructor(ref: ts.Identifier) {
     const messages = [
-      `Currently only the following built-in Hooks are supported: ${BuiltinHooks.join(',')}`,
-      `variable: ${ref.getText()}`,
-      `sourcefile: ${getSourceFilePath(ref)}`,
+      buildErrorCodeFrame(ref, `Only built-in Hooks are supported. built-in hooks: ${BuiltinHooks.join(', ')}`),
+      `\n\nsourcefile: ${relative(helper.root, getSourceFilePath(ref))}`,
     ]
     super(messages.join('\n'))
-    this.name = 'BuiltinHooksError'
   }
 }
