39678d0323d040aa29cef7e29a23cbf6570ac779
Lxxyx <Lxxyxzj@gmail.com>
2020-11-11 17:17:29 +0800

    🚨 test(cov): import code cov

--- packages/next-hooks-compiler/src/__tests__/fixtures/with-controller/src/middleware/class.ts
+++ packages/next-hooks-compiler/src/__tests__/fixtures/with-controller/src/middleware/class.ts
@@ -3,7 +3,7 @@ import { FaaSContext } from '@midwayjs/faas'
 
 @Provide('classMiddleware')
 @Scope(ScopeEnum.Singleton)
-export class FooMiddleware {
+export class ClassMiddleware {
   resolve() {
     return async (ctx: FaaSContext, next: () => Promise<void>) => {
       ctx.query.from = 'classMiddleware'
