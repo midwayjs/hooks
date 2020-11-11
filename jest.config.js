43b2b945d56714296c915ea9ad8ca7461911683f
Lxxyx <Lxxyxzj@gmail.com>
2020-11-11 19:36:55 +0800

    🚨 test(jest): increase timeout & use ci

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/fixtures/', 'util.ts', 'compiler.ts'],
-  testTimeout: 30000,
+  testTimeout: 120 * 1000,
   forceExit: true,
   coveragePathIgnorePatterns: ['/node_modules/', '.faas_debug_tmp', '__tests__'],
   snapshotSerializers: ['jest-serializer-path', 'jest-snapshot-serializer-raw'],
