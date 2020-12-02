7528aefa7c9cdc43ea406f57b2c1c77fea939b67
Lxxyx <Lxxyxzj@gmail.com>
2020-12-02 14:09:54 +0800

    🔨 refactor(hooks): useInject default type is any

--- packages/hooks/src/api/useInject.ts
+++ packages/hooks/src/api/useInject.ts
@@ -2,7 +2,7 @@ import { useContext } from './useContext'
 export function useInject<T>(identifier: new () => T): Promise<T>
 export function useInject<T>(identifier: string): Promise<T>
 
-export async function useInject<T>(identifier: (new () => T) | string) {
+export async function useInject<T = any>(identifier: (new () => T) | string) {
   const { requestContext } = useContext()
   return requestContext.getAsync<T>(identifier)
 }
