2f759590fc29af31617f11f1552dbcef490b5a35
Lxxyx <Lxxyxzj@gmail.com>
2020-12-02 14:47:02 +0800

    🐛 fix(hooks): useInject typings

--- packages/hooks/src/api/useInject.ts
+++ packages/hooks/src/api/useInject.ts
@@ -1,6 +1,4 @@
 import { useContext } from './useContext'
-export function useInject<T>(identifier: new () => T): Promise<T>
-export function useInject<T>(identifier: string): Promise<T>
 
 export async function useInject<T = any>(identifier: (new () => T) | string) {
   const { requestContext } = useContext()
