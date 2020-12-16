01294d4a65656794f6d3c3f85c3518300302e79f
Lxxyx <Lxxyxzj@gmail.com>
2020-12-16 15:03:14 +0800

    🔨 refactor(sync): update sync script

--- scripts/sync_status.js
+++ scripts/sync_status.js
@@ -1,34 +1,23 @@
-const { execSync } = require('child_process')
 const execa = require('execa')
-
-const originData = execSync('npx lerna ls --json').toString()
-const data = JSON.parse(originData)
 
 const failed = []
 const finished = []
 
-async function checkSyncStatus(pkg) {
-  const npmVersion = await execa('npm', ['show', pkg, 'version'])
-  const tnpmVersion = await execa('tnpm', ['show', pkg, 'version'])
-  const cnpmVersion = await execa('cnpm', ['show', pkg, 'version'])
+async function checkSyncStatus(packages, pkg, clients) {
+  const versions = await Promise.all(clients.map((client) => execa(client, ['show', pkg, 'version'])))
 
   finished.push(pkg)
-  console.log(`[${finished.length}/${data.length}] ---->`, pkg)
-  if (npmVersion.stdout !== tnpmVersion.stdout || npmVersion.stdout !== cnpmVersion.stdout) {
-    console.log(`===> npm: ${npmVersion.stdout}, tnpm: ${tnpmVersion.stdout}, cnpm: ${cnpmVersion.stdout}`)
+  console.log(`[${finished.length}/${packages.length}] ---->`, pkg)
+
+  const isSuccess = versions.every((version) => version.stdout === versions[0].stdout)
+
+  if (!isSuccess) {
+    console.log(`===> ${clients.map((client, i) => `${client}: ${versions[i].stdout}`).join(' ')}`)
     failed.push(pkg)
   }
 }
 
-async function start() {
-  const packages = data.filter((item) => item.private === false).map((item) => item.name)
-
-  const task = packages.map((pkg) => checkSyncStatus(pkg))
+module.exports = async function check(packages, clients) {
+  const task = packages.map((pkg) => checkSyncStatus(packages, pkg, ['npm', ...clients]))
   await Promise.all(task)
-
-  if (failed.length) {
-    console.log(`output command => tnpm sync ${failed.join(' ')}`)
-  }
 }
-
-start()
