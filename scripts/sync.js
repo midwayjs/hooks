9c585eed6dcf95e9fedea664556fb6d41787f5fa
Lxxyx <Lxxyxzj@gmail.com>
2020-12-16 15:03:14 +0800

    🔨 refactor(sync): update sync script

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,30 +1,34 @@
 const { execSync } = require('child_process')
 const execa = require('execa')
 const { resolve } = require('path')
+const check = require('./sync_status')
 
 const originData = execSync('npx lerna ls --json').toString()
 const data = JSON.parse(originData)
 const finished = []
 
-async function syncPackage(pkg) {
-  await Promise.all([execa('tnpm', ['sync', pkg]), execa('cnpm', ['sync', pkg])])
+const clients = process.argv.slice(2)
+
+async function syncPackage(pkg, clients) {
+  const tasks = clients.map((client) => execa(client, ['sync', pkg]))
+  await Promise.all(tasks)
+
   finished.push(pkg)
   console.log(`[${finished.length}/${data.length}] ${pkg} sync finished`)
 }
 
-async function sync() {
+async function sync(clients) {
   console.log('\n=== start sync ===\n')
   const packages = data.map((item) => item.name)
 
   console.log(`sync ${packages.length} packages:\n${packages.join('\n')}\n`)
-  const task = packages.map((pkg) => syncPackage(pkg))
+  const task = packages.map((pkg) => syncPackage(pkg, clients))
   await Promise.all(task)
 
   console.log('\n=== check sync status ===\n')
-  await execa('node', [resolve(__dirname, 'sync_status.js')], { stdio: 'inherit' })
+  await check(packages, clients)
 
   console.log('\n=== sync finished ===')
-  process.exit(1)
 }
 
-sync()
+sync(clients)
