858346a4a6c389b15cd187b7fcd1f43850d96969
Lxxyx <Lxxyxzj@gmail.com>
2021-01-04 14:29:38 +0800

    ✨ feat(typescript): upgrade to 4.1 (#39)

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/fixtures/', 'util.ts', 'compiler.ts'],
   testTimeout: 120 * 1000,
   forceExit: true,
+  testPathIgnorePatterns: ['/node_modules/', '/fixtures/', 'util.ts', 'compiler.ts'],
   coveragePathIgnorePatterns: ['/node_modules/', '.faas_debug_tmp', '__tests__'],
   snapshotSerializers: ['jest-serializer-path', 'jest-snapshot-serializer-raw'],
 }
