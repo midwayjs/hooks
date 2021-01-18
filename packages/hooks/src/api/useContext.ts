02feea1fa2bbfef1e94f64ad3be3b1b0ec2f675c
Lxxyx <Lxxyxzj@gmail.com>
2021-01-18 11:54:22 +0800

    hooks: remove version logic

--- packages/hooks/src/api/useContext.ts
+++ packages/hooks/src/api/useContext.ts
@@ -1,11 +1,10 @@
 import { FaaSContext } from '@midwayjs/faas'
-import semver from 'semver'
 
 export let useContext: () => FaaSContext
 
 // HOOKS_RUNTIME = 'async_hooks' || node version < 12.17.0
-if (process.env.HOOKS_RUNTIME !== 'async_hooks' || semver.lt(process.version, '12.17.0')) {
-  useContext = require('./compiler/useContextImpl').useContext
-} else {
+if (process.env.HOOKS_RUNTIME === 'async_hooks') {
   useContext = require('./async_hooks/useContextImpl').useContext
+} else {
+  useContext = require('./compiler/useContextImpl').useContext
 }
