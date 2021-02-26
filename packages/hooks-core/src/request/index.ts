a6344746930dd6677c7b11c02a16d5d0a67ef1e9
Lxxyx <Lxxyxzj@gmail.com>
2021-02-26 13:00:37 +0800

    core: refactor request to core

---
+++ packages/hooks-core/src/request/index.ts
@@ -0,0 +1,9 @@
+import { LambdaParam } from '../types/http'
+import axios from 'axios'
+
+export const defaults = axios.defaults
+
+export async function request(param: LambdaParam) {
+  const response = await axios(param)
+  return response.data
+}
