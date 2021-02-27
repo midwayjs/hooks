9df710194a802d6778eb4e132739b1c6c2c15b08
Lxxyx <Lxxyxzj@gmail.com>
2021-02-27 17:37:03 +0800

    core: support load ts export default syntax

--- packages/hooks-core/src/config/__tests__/config.test.ts
+++ packages/hooks-core/src/config/__tests__/config.test.ts
@@ -1,35 +1,20 @@
 import { getConfig } from '../'
 import { join } from 'path'
 
-describe('user config', () => {
-  test('load js config', () => {
-    const config = loadConfig('js')
-    expect(config).toMatchInlineSnapshot(`
-      Object {
-        "routes": Array [
-          Object {
-            "baseDir": "lambda",
-            "basePath": "/api",
-          },
-        ],
-        "source": "/src",
-      }
-    `)
-  })
+const userConfig = {
+  source: '/src',
+  routes: [
+    {
+      baseDir: 'lambda',
+      basePath: '/api',
+    },
+  ],
+}
 
-  test('load ts config', () => {
-    const config = loadConfig('ts')
-    expect(config).toMatchInlineSnapshot(`
-      Object {
-        "routes": Array [
-          Object {
-            "baseDir": "lambda",
-            "basePath": "/api",
-          },
-        ],
-        "source": "/src",
-      }
-    `)
+test('load user config', () => {
+  const fixtures = ['js', 'ts', 'ts-export-default']
+  fixtures.forEach((fixture) => {
+    expect(loadConfig(fixture)).toEqual(userConfig)
   })
 })
 
