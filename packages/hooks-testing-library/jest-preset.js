7fe29f14cf119aa1fd44a09bef7c2690e5465f2e
Lxxyx <Lxxyxzj@gmail.com>
2021-02-28 15:37:46 +0800

    test: add hooks-testing-library

---
+++ packages/hooks-testing-library/jest-preset.js
@@ -0,0 +1,38 @@
+const common = {
+  preset: 'ts-jest',
+  clearMocks: true,
+  testPathIgnorePatterns: ['/node_modules/', '/.faas_debug_tmp/'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'esbuild-jest',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  modulePathIgnorePatterns: ['<rootDir>/.faas_debug_tmp', '<rootDir>/run'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+}
+
+module.exports = {
+  preset: 'ts-jest',
+  // maxWorkers: 1,
+  projects: [
+    {
+      ...common,
+      name: 'server',
+      displayName: {
+        name: 'SERVER',
+        color: 'magenta',
+      },
+      testEnvironment: 'node',
+      testRegex: [
+        '\\.server\\.(spec|test)\\.(j|t)sx?$',
+        '[\\/](apis)[\\/].*\\.(test|spec)\\.(j|t)sx?$',
+      ],
+      // setupFilesAfterEnv: [
+      //   "<rootDir>/test/setup.ts",
+      // ],
+    },
+  ],
+}
