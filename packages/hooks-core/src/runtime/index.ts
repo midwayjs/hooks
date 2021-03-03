427cf2bbc997ed5a44e1ac51e885c811e808b2b3
Lxxyx <Lxxyxzj@gmail.com>
2021-03-03 17:47:09 +0800

    core: use AsyncLocalStorage polyfill for lower node versions

--- packages/hooks-core/src/runtime/index.ts
+++ packages/hooks-core/src/runtime/index.ts
@@ -1,4 +1,4 @@
-import { AsyncLocalStorage } from 'async_hooks'
+import { als as AsyncLocalStoragePolyfill } from 'asynchronous-local-storage'
 
 export type HooksContext = {
   ctx: any
@@ -9,4 +9,21 @@ export type HooksContext = {
  * @private
  * private api, may change without notice.
  */
-export const als = new AsyncLocalStorage<HooksContext>()
+export const als = {
+  getStore(key: string) {
+    return AsyncLocalStoragePolyfill.get<any>(key)
+  },
+  run(ctx: HooksContext, callback: any) {
+    return new Promise((resolve) => {
+      AsyncLocalStoragePolyfill.runWith(async () => {
+        resolve(await callback())
+      }, ctx)
+    })
+  },
+}
+
+// /**
+//  * @private
+//  * private api, may change without notice.
+//  */
+// export const als = new AsyncLocalStorage<HooksContext>()
