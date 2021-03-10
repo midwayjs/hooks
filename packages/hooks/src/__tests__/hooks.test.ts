e18897b032b594db31490fa11db80ce9d3689c5f
Lxxyx <Lxxyxzj@gmail.com>
2021-03-10 19:10:57 +0800

    test: add test case for hooks

---
+++ packages/hooks/src/__tests__/hooks.test.ts
@@ -0,0 +1,50 @@
+import { als } from '@midwayjs/hooks-core'
+import {
+  useConfig,
+  useContext,
+  useInject,
+  useLogger,
+  usePlugin,
+} from '@midwayjs/hooks'
+
+test('', () => {
+  const mockContext = {
+    logger: jest.fn(),
+    app: {
+      mysql: jest.fn(),
+    },
+    cors: jest.fn(),
+    requestContext: {
+      getAsync: jest.fn(),
+      getConfigService() {
+        return {
+          getConfiguration(key) {
+            return key
+          },
+        }
+      },
+    },
+  }
+
+  als.run({ ctx: mockContext }, async () => {
+    expect(useContext()).toStrictEqual(mockContext)
+
+    const logger = useLogger()
+    logger()
+    expect(mockContext.logger).toHaveBeenCalledTimes(1)
+
+    const mysql = usePlugin('mysql')
+    mysql()
+    expect(mockContext.app.mysql).toHaveBeenCalledTimes(1)
+
+    const cors = usePlugin('cors')
+    cors()
+    expect(mockContext.cors).toHaveBeenCalledTimes(1)
+
+    await useInject('testService')
+    expect(mockContext.requestContext.getAsync).toHaveBeenCalledTimes(1)
+
+    const config = useConfig('test')
+    expect(config).toStrictEqual('test')
+  })
+})
