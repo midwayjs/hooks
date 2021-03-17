a958f81e36987e8d9152da9e30336d51c95bdc72
Lxxyx <Lxxyxzj@gmail.com>
2021-03-17 15:59:40 +0800

    core: support hooks style middleware

--- examples/middleware/src/middleware.ts
+++ examples/middleware/src/middleware.ts
@@ -1,7 +1,9 @@
 import { Context, IMidwayKoaNext } from '@midwayjs/koa';
+import { useContext } from '@midwayjs/hooks';
 
 export function createLogger(level: string) {
-  return async (ctx: Context, next: IMidwayKoaNext) => {
+  return async (next: IMidwayKoaNext) => {
+    const ctx = useContext<Context>();
     if (process.env.NODE_ENV !== 'test') {
       console.log(`[${level}] <-- [${ctx.method}] ${ctx.url}`);
     }
