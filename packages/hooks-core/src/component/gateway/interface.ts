b57a33dfdecaae21da21b1288ce98ab96d5182ee
Lxxyx <Lxxyxzj@gmail.com>
2021-04-14 15:37:10 +0800

    core: support gateway adapter

---
+++ packages/hooks-core/src/component/gateway/interface.ts
@@ -0,0 +1,20 @@
+import { ApiFunction } from '@midwayjs/hooks-core'
+import { ServerRouter } from '../../router'
+import { InternalConfig, RuntimeConfig, ServerRoute } from '../../types/config'
+
+export interface CreateApiParam {
+  fn: ApiFunction
+  file: string
+  isExportDefault: boolean
+}
+
+export type ComponentConfig = {
+  runtime: RuntimeConfig
+  internal: InternalConfig
+  router: ServerRouter
+  root: string
+}
+
+export type Class<T = unknown, Arguments extends any[] = any[]> = new (
+  ...arguments_: Arguments
+) => T
