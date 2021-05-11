69baa5841ebe0f2ea9fd01b5cd574d0d9759a505
Lxxyx <Lxxyxzj@gmail.com>
2021-05-11 19:12:35 +0800

    babel: remove warning

--- babel.config.js
+++ babel.config.js
@@ -10,5 +10,8 @@ module.exports = {
       },
     ],
   ],
-  plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+  ],
 }
