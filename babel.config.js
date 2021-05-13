da1bf5c894ad8e243983808f3812082fa3c33b61
Lxxyx <Lxxyxzj@gmail.com>
2021-05-13 15:01:53 +0800

    babel: enable optional-chaining and fix tests

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = {
     ],
   ],
   plugins: [
+    ['@babel/plugin-proposal-optional-chaining'],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     ['@babel/plugin-proposal-private-methods', { loose: true }],
   ],
