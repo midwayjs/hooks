64a9845903b52f1d5b5bc840aa0e5df60471e7a1
Lxxyx <Lxxyxzj@gmail.com>
2021-05-14 17:01:40 +0800

    core: ignore env detect case

--- packages/hooks-core/src/util.ts
+++ packages/hooks-core/src/util.ts
@@ -1,11 +1,14 @@
 export function isDevelopment() {
   if (
     process.env.MIDWAY_TS_MODE === 'true' ||
+    /* istanbul ignore next */
     process.env.NODE_ENV === 'test' ||
+    /* istanbul ignore next */
     process.env.NODE_ENV === 'development'
   ) {
     return true
   }
 
+  /* istanbul ignore next */
   return false
 }
