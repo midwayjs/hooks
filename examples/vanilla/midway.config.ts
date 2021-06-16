4fa63ce3710dfebc09df9e658707536fefe884dc
Lxxyx <Lxxyxzj@gmail.com>
2021-06-16 16:32:41 +0800

    example: rename function to server

--- examples/vanilla/midway.config.ts
+++ examples/vanilla/midway.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@midwayjs/hooks';
 
 export default defineConfig({
   superjson: true,
-  source: 'function',
+  source: 'server',
   routes: [
     {
       baseDir: 'api',
