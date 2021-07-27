6002c14aed5903f85a79831858205ce95b99cd22
Lxxyx <Lxxyxzj@gmail.com>
2021-07-27 14:08:12 +0800

    core: refactor to use createFunctionContainer

--- packages/hooks-core/src/runtime/index.ts
+++ packages/hooks-core/src/runtime/index.ts
@@ -1,4 +1,9 @@
 import { als as AsyncLocalStoragePolyfill } from 'asynchronous-local-storage'
+import { __decorate } from 'tslib'
+
+import { Inject, Provide } from '@midwayjs/decorator'
+
+import { ApiFunction } from '..'
 
 export type HooksContext = {
   ctx: any
@@ -25,4 +30,49 @@ export const als = {
 
 export function useContext<T = any>(): T {
   return als.getStore('ctx')
+}
+
+type CreateFunctionContainerOptions = {
+  fn: ApiFunction
+  functionId: string
+  parseArgs: (ctx: any, ...args: any) => any[]
+  handlerDecorators?: any[]
+  classDecorators?: any[]
+  isHTTP?: boolean
+}
+
+export function createFunctionContainer(
+  options: CreateFunctionContainerOptions
+) {
+  const {
+    fn,
+    functionId,
+    parseArgs,
+    handlerDecorators = [],
+    classDecorators = [],
+    isHTTP,
+  } = options
+
+  let FunctionContainer = class FunctionContainer {
+    ctx: any
+    async handler(...handlerArgs: any[]) {
+      const args = parseArgs(this.ctx, ...handlerArgs)
+      return isHTTP
+        ? await fn(...args)
+        : await als.run({ ctx: this.ctx }, async () => await fn(...args))
+    }
+  }
+
+  __decorate([Inject()], FunctionContainer.prototype, 'ctx', void 0)
+  __decorate(
+    [...handlerDecorators],
+    FunctionContainer.prototype,
+    'handler',
+    null
+  )
+  FunctionContainer = __decorate(
+    [Provide(functionId), ...classDecorators],
+    FunctionContainer
+  )
+  return FunctionContainer
 }
