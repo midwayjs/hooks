ce4af852db8e8448105175a7826ef01664e2fdb4
Lxxyx <Lxxyxzj@gmail.com>
2021-08-04 14:23:15 +0800

    core & hooks: remove useless deps

--- packages/hooks-core/src/gateway/http/logger.ts
+++ packages/hooks-core/src/gateway/http/logger.ts
@@ -1,5 +1,3 @@
-/* istanbul ignore file */
-import chalk from 'chalk'
 import { relative } from 'upath'
 
 export function duplicateLogger(
@@ -10,9 +8,9 @@ export function duplicateLogger(
 ) {
   console.log(
     '[%s] Duplicate routes detected. %s and %s both resolve to %s.',
-    chalk.yellow('warn'),
-    chalk.cyan(relative(root, existPath)),
-    chalk.cyan(relative(root, currentPath)),
-    chalk.cyan(api)
+    'warn',
+    relative(root, existPath),
+    relative(root, currentPath),
+    api
   )
 }
