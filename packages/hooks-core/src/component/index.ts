7a5211dacd792012d3bddc26a1644934f8650df4
Lxxyx <Lxxyxzj@gmail.com>
2021-08-05 14:07:12 +0800

    core: support load from snapshot

--- packages/hooks-core/src/component/index.ts
+++ packages/hooks-core/src/component/index.ts
@@ -1,6 +1,8 @@
 import { getConfig, getProjectRoot } from '../config'
 import { FileRouter } from '../router/file'
+import { getSnapshot } from '../snapshot'
 import { RuntimeConfig } from '../types/config'
+import { ComponentOptions } from '../types/gateway'
 import { isDevelopment } from '../util'
 import { validateArray } from '../validator'
 import { HooksComponent } from './component'
@@ -13,20 +15,29 @@ export const hooks = (runtimeConfig: RuntimeConfig = {}) => {
     validateArray(runtimeConfig.middleware, 'runtimeConfig.middleware')
   }
 
-  const root = getProjectRoot()
-  const projectConfig = getConfig()
+  const options: Partial<ComponentOptions> = {}
+  const snapshot = getSnapshot()
+
+  if (snapshot) {
+    options.root = snapshot.root
+    options.projectConfig = snapshot.projectConfig
+  } else {
+    options.root = getProjectRoot()
+    options.projectConfig = getConfig()
+  }
+
   const router = new FileRouter({
-    root,
-    projectConfig,
+    root: options.root,
+    projectConfig: options.projectConfig,
     useSourceFile: isDevelopment(),
   })
 
   const hooks = new HooksComponent({
-    root,
+    root: options.root,
     router,
     runtimeConfig,
-    projectConfig,
+    projectConfig: options.projectConfig,
   })
 
-  return hooks.init()
+  return hooks.load()
 }
--- packages/hooks-core/src/request/index.ts
+++ packages/hooks-core/src/request/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios'
+import axios from 'axios'
 
 import type { ApiParam } from '../types/http'
 
