2d2e5384135c8ab823e4d9966896b7084dc85e81
Lxxyx <Lxxyxzj@gmail.com>
2021-08-14 11:07:05 +0800

    core: add snapshot test

--- packages/hooks-core/src/runtime/snapshot.ts
+++ packages/hooks-core/src/runtime/snapshot.ts
@@ -1,5 +1,8 @@
+import { uniq } from 'lodash'
+
 import { IMidwayContainer } from '@midwayjs/core'
 
+import { getBuiltInGateways } from '../'
 import { ApiModule } from '../types/common'
 import { ProjectConfig } from '../types/config'
 
@@ -19,6 +22,11 @@ export const SNAPSHOT_SYMBOL = Symbol.for('MIDWAY_HOOKS_SNAPSHOT')
 export const SNAPSHOT = globalThis[SNAPSHOT_SYMBOL]
 
 export function createSnapshot(snapshot: SnapShot) {
+  snapshot.projectConfig.gateway ??= []
+  snapshot.projectConfig.gateway.push(
+    ...getBuiltInGateways(snapshot.projectConfig)
+  )
+  snapshot.projectConfig.gateway = uniq(snapshot.projectConfig.gateway)
   globalThis[SNAPSHOT_SYMBOL] = snapshot
 }
 
