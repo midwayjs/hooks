8fc0b16e66a9f4d6e1a7da021b7a29fdd7472b3c
Lxxyx <Lxxyxzj@gmail.com>
2022-01-21 14:39:03 +0800

    feat: add getHttpTrigger for @midwayjs/rpc

--- packages/rpc/src/index.ts
+++ packages/rpc/src/index.ts
@@ -1,4 +1,4 @@
 export * from './http'
-export { args } from './util'
+export { args, getHttpTrigger } from './util'
 export { setupHttpClient } from './client'
 export type { Context, Middleware, SetupOptions } from './client'
