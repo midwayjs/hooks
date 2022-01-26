ff7f56cea8bb9a6f9dcfdb7371db4544f07385e3
Lxxyx <Lxxyxzj@gmail.com>
2022-01-26 11:45:29 +0800

    fix: use relative directory for render

--- packages/hooks-kit/src/command/build.ts
+++ packages/hooks-kit/src/command/build.ts
@@ -92,17 +92,18 @@ async function executePromise(promise: Promise<any> | any) {
 }
 
 function createRender(dist: string) {
-  const clientDir = join(dist, '_client')
   const code = `
   const { Api, Get, Middleware } = require('@midwayjs/hooks');
   const cache = require('koa-static-cache');
   const path = require('path');
+  
+  const client = path.join(__dirname, '_client');
 
   exports.default = Api(
     Get('/*'),
     Middleware(
       cache({
-        dir: '${clientDir}',
+        dir: client,
         dynamic: true,
         alias: { '/': 'index.html' },
         buffer: true,
