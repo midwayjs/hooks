f8fa8658b2056799d7a23df259b90ccb9c1a2ae1
Lxxyx <Lxxyxzj@gmail.com>
2022-02-11 15:11:20 +0800

    refactor: use @midwayjs/asynchronous-local-storage

--- packages/hooks-core/src/runtime/index.ts
+++ packages/hooks-core/src/runtime/index.ts
@@ -1,26 +1,15 @@
 import { AsyncLocalStorageRuntime } from './AsyncLocalStorage'
 import { HooksContext } from './type'
-import {
-  isEnableSingleLocalStorage,
-  SingletonLocalStorageRuntime,
-} from './SingletonLocalStorage'
 
 export * from './type'
 export * from './AsyncLocalStorage'
-export * from './SingletonLocalStorage'
 
 export const ContextManager = {
-  get runtime() {
-    if (isEnableSingleLocalStorage()) {
-      return SingletonLocalStorageRuntime
-    }
-    return AsyncLocalStorageRuntime
-  },
   getValue(key: string) {
-    return ContextManager.runtime.getValue(key)
+    return AsyncLocalStorageRuntime.getValue(key)
   },
   run(ctx: HooksContext, callback: () => Promise<any>) {
-    return ContextManager.runtime.run(ctx, callback)
+    return AsyncLocalStorageRuntime.run(ctx, callback)
   },
 }
 
