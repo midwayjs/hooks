6f3e96da747be8aa38849d5f01e094439bbb2bbd
Lxxyx <Lxxyxzj@gmail.com>
2022-02-22 19:53:05 +0800

    fix: use midway.config.js for api server project, fix #195

--- examples/api/scripts/bootstrap.js
+++ examples/api/scripts/bootstrap.js
@@ -1,9 +1,9 @@
 const { Bootstrap } = require('@midwayjs/bootstrap');
 const { resolve } = require('path');
 
-Bootstrap.configure({
-  baseDir: resolve(__dirname, '../dist'),
-})
+const baseDir = resolve(__dirname, '../dist');
+
+Bootstrap.configure(baseDir)
   .run()
   .then(() => {
     console.log('Your application is running at http://localhost:7001');
