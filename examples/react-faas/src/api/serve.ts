aae4b0102036f91a93bc077a22bce442d10094f0
Lxxyx <Lxxyxzj@gmail.com>
2022-03-02 16:21:25 +0800

    fix: serve html in non-development env

--- examples/react-faas/src/api/serve.ts
+++ examples/react-faas/src/api/serve.ts
@@ -4,6 +4,6 @@ import { Serve } from '@midwayjs/serve';
  * Serve html in production mode
  */
 const Handler =
-  process.env.NODE_ENV === 'production' && Serve('/*', { dir: 'build' });
+  process.env.NODE_ENV !== 'development' && Serve('/*', { dir: 'build' });
 
 export default Handler;
