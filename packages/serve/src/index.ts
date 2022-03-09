29acef267af237506ec4196be03bff85619a4761
Lxxyx <Lxxyxzj@gmail.com>
2022-03-09 11:33:04 +0800

    feat: support spa for hooks-kit

--- packages/serve/src/index.ts
+++ packages/serve/src/index.ts
@@ -12,32 +12,44 @@ const defaultOptions: ServeOptions = {
   buffer: true,
 }
 
-export const serve = (options: ServeOptions) => {
-  let middleware: ReturnType<typeof KoaStaticCache>
+async function createMiddleware(ctx: any, options: ServeOptions) {
+  const baseDir: string = await ctx.requestContext.get('baseDir')
+  const dir = options.isKit
+    ? join(baseDir, options.dir)
+    : join(baseDir, '..', options.dir)
 
+  return KoaStaticCache({
+    ...defaultOptions,
+    ...options,
+    dir,
+  })
+}
+
+export const serve = (options: ServeOptions) => {
+  let middleware: ReturnType<typeof KoaStaticCache> = null
   return async (next: any) => {
     const ctx = useContext()
-
-    if (!middleware) {
-      const baseDir: string = await ctx.requestContext.get('baseDir')
-      options.dir = options.isKit
-        ? join(baseDir, options.dir)
-        : join(baseDir, '..', options.dir)
-
-      middleware = KoaStaticCache({
-        ...defaultOptions,
-        ...options,
-      })
-    }
-
+    middleware ??= await createMiddleware(ctx, options)
     return middleware(ctx, next)
   }
 }
 
-export function Serve(url: string, options: Options) {
+export function Serve(url: string, options: ServeOptions) {
   return Api(Get(url), Middleware(serve(options)), async () => {
     const ctx = useContext()
-    ctx.status = 404
-    return 'Not Found'
+
+    if (!options.isKit) {
+      ctx.status = 404
+      return 'Not Found'
+    }
+
+    const forkedCtx = new Proxy(ctx, {
+      get(target, property, receiver) {
+        if (property === 'path') return '/'
+        return Reflect.get(target, property, receiver)
+      },
+    })
+    const mw = await createMiddleware(forkedCtx, options)
+    await mw(forkedCtx, async () => {})
   })
 }
