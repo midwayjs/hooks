3e9fd1ced7c6b485b4d3a20cbb22536ecd8cfe59
Lxxyx <Lxxyxzj@gmail.com>
2022-04-25 11:22:30 +0800

    chore: add script to generate package

---
+++ scripts/create-pkg.ts
@@ -0,0 +1,34 @@
+import { readFile, cp, writeFile } from 'node:fs/promises'
+import path from 'node:path'
+import { sync } from 'globby'
+
+function validateArgs(args: string[]) {
+  if (typeof args[0] !== 'string') {
+    throw new Error('First argument must be a package name')
+  }
+}
+
+// get arguments
+const args = process.argv.slice(2)
+validateArgs(args)
+
+const packageName = args[0]
+const template = path.resolve(__dirname, '../template')
+const target = path.resolve(__dirname, '../packages', packageName)
+
+async function createPackage() {
+  await cp(template, target, { recursive: true })
+
+  for (const file of sync(['**/*'], {
+    cwd: target,
+    absolute: true,
+  })) {
+    const content = await readFile(file, 'utf-8')
+    const newContent = content.replace(/\{packageName\}/g, packageName)
+    await writeFile(file, newContent, 'utf-8')
+  }
+
+  console.log(`Package @midwayjs/${packageName} created successfully!`)
+}
+
+createPackage()
