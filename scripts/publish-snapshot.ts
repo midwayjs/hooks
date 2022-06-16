c5a83c769b811bd0c0cc2d6c4b36cda1e855b99c
Lxxyx <Lxxyxzj@gmail.com>
2022-06-16 15:35:06 +0800

    refactor: auto generate changeset from packages

--- scripts/publish-snapshot.ts
+++ scripts/publish-snapshot.ts
@@ -1,9 +1,10 @@
-import { copyFile } from 'node:fs/promises'
+import { writeFile } from 'node:fs/promises'
 import path from 'node:path'
 import { command } from 'execa'
 import assert from 'node:assert'
+import { execSync } from 'node:child_process'
+import dedent from 'dedent'
 
-const template = path.resolve(__dirname, '../template/changeset.md')
 const dist = path.resolve(__dirname, '../.changeset')
 const cwd = path.resolve(__dirname, '..')
 
@@ -11,9 +12,26 @@ const tag = process.argv[2]
 assert(tag, 'Tag is required')
 assert(typeof tag === 'string', 'Tag must be a string')
 
+type LernaPackage = {
+  name: string
+  version: string
+  private: boolean
+  location: string
+}
+
 async function publishSnapshot() {
   console.log(`Generate snapshot changeset for ${tag}`)
-  await copyFile(template, path.resolve(dist, `changeset-${Date.now()}.md`))
+  const pkgs: LernaPackage[] = JSON.parse(
+    execSync('npx lerna ls --json').toString()
+  )
+  const publicPkgs = pkgs
+    .filter((pkg) => !pkg.private && pkg.name.startsWith('@midwayjs'))
+    .map((pkg) => pkg.name)
+  const changeset = createChangeset(publicPkgs)
+
+  await writeFile(path.resolve(dist, `changeset-${Date.now()}.md`), changeset, {
+    encoding: 'utf-8',
+  })
   await command(`yarn changeset version --snapshot ${tag}`, {
     cwd,
     stdio: 'ignore',
@@ -31,6 +49,16 @@ async function publishSnapshot() {
   await command(`git checkout -- */CHANGELOG.md`, { cwd })
   await command(`git checkout -- packages/*/package.json`, { cwd })
   console.log(`\nSnapshot changeset published`)
+}
+
+function createChangeset(pkgs: string[]) {
+  return dedent`
+  ---
+  ${pkgs.map((pkg) => `'${pkg}': patch`).join('\n')}
+  ---
+  
+  feat: add snapsnot changeset
+  `
 }
 
 publishSnapshot()
