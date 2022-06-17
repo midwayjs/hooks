38e30a1b9fa080f3b89790da3d93855304142437
Lxxyx <Lxxyxzj@gmail.com>
2022-06-17 10:36:58 +0800

    feat: hcc support get preload code by api

---
+++ packages/hcc/src/index.ts
@@ -0,0 +1,14 @@
+import { getSource, getRouter, setProjectRoot } from '@midwayjs/hooks/internal'
+import { GenerateTarget, getEntryCode } from './midway'
+
+export async function getPreloadCode(cwd: string) {
+  setProjectRoot(cwd)
+
+  const source = getSource({ useSourceFile: false })
+  const router = getRouter(source)
+
+  return `
+    ${getEntryCode(source, router, GenerateTarget.JS)}
+    module.exports = require('./configuration');
+  `
+}
