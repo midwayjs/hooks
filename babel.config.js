0c89694ecd478306f4f48bec2371575a52c1c2b8
Lxxyx <Lxxyxzj@gmail.com>
2022-11-01 16:09:22 +0800

    refactor: use lerna v6 and new babel config

--- babel.config.js
+++ babel.config.js
@@ -12,10 +12,4 @@ module.exports = {
       },
     ],
   ],
-  plugins: [
-    ['@babel/plugin-proposal-optional-chaining'],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-  ],
 }
