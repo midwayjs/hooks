a716f470ac9a1a5c045513d16174d4279e6b3b14
Lxxyx <Lxxyxzj@gmail.com>
2022-12-14 14:13:58 +0800

    refactor: remove useless files

--- examples/vue-faas/serve.ts
+++
@@ -1,9 +0,0 @@
-import { Serve } from '@midwayjs/serve';
-
-/**
- * Serve html in production mode
- */
-const Handler =
-  process.env.NODE_ENV !== 'development' && Serve('/*', { dir: 'build' });
-
-export default Handler;
