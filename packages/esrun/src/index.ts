75014614bfe64d966609756430948936595b57ff
Lxxyx <Lxxyxzj@gmail.com>
2023-02-13 18:52:34 +0800

    fix: use commonjs by default for esrun (#521)

--- packages/esrun/src/index.ts
+++ packages/esrun/src/index.ts
@@ -6,6 +6,9 @@ export function register() {
 
   // ensure all files are compiled
   options.files = null
+  // transpile to CommonJS by Default
+  options.module = 'CommonJS' as any
+
   registerSWC(options)
 
   if (options.paths && Object.keys(options.paths).length) {
